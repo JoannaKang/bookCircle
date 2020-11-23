d8f245a49d76466667915730bce923fa55c0d17e
Joanna Kang <jfrences11@gmail.com>
2020-11-23 14:23:31 +0000

    fix: fix typescript issue in App.tsx file

---
+++ client/src/ApiService/serverApiService.tsx
@@ -0,0 +1,182 @@
+import { User, ActivityLog, Book, AddFriend, BookRequest, NewBook } from '../Interfaces'
+
+const URL = 'http://localhost:3001/'
+
+async function createUser(data: object) {
+  console.log(data, 'API!');
+  //should send data to backend /createUser
+
+}
+
+async function getUser(name: string): Promise<User> {
+  const user = await fetch(URL + 'getUser/' + name)
+    .then(response => response.json())
+
+  return user[0];
+}
+
+async function getFriendName(name: string): Promise<Object> {
+  const friendsName = await fetch(URL + 'getFriendsNames/' + name)
+    .then(response => response.json())
+  return friendsName.name;
+}
+
+async function searchFriend(name: string): Promise<Object> {
+  const friend = await fetch(URL + 'searchFriend/' + name)
+    .then(response => response.json())
+  return friend;
+}
+
+async function addFriend(obj: AddFriend): Promise<User> {
+  const result = await fetch(URL + 'addFriend', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(obj)
+  })
+    .then(response => response.json())
+  return result;
+}
+
+async function acceptFriend(activity: ActivityLog): Promise<User> {
+  const result = await fetch(URL + 'confirmFriend', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(activity)
+  })
+    .then(response => response.json())
+  return result;
+}
+
+async function rejectFriend(activity: ActivityLog): Promise<User> {
+  const result = await fetch(URL + 'rejectFriendRequest', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(activity)
+  })
+    .then(response => response.json())
+  return result;
+}
+
+async function addBook(newbook: NewBook) {
+  const result = await fetch(URL + 'addBook', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newbook)
+  })
+    .then(response => response.json())
+
+  return result
+}
+
+async function deleteBook(userId: string, bookId: string) {
+  const result = await fetch(URL + 'deleteBook', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ userId, bookId })
+  }).then(response => response.json())
+  return result;
+}
+
+async function updateBook(userId: string, bookId: string, newBook: Book) {
+  const result = await fetch(URL + 'editBook', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ userId, bookId, newBook })
+  }).then(response => response.json())
+  return result;
+}
+
+async function getAvailableBooks(id: string | null) {
+  return await fetch(URL + 'availableBooks/' + id)
+    .then(response => response.json())
+}
+
+async function requestBook(obj: BookRequest) {
+  const result = await fetch(URL + 'requestBook', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(obj)
+  })
+    .then(response => response.json())
+  return result;
+}
+
+async function acceptBookRequest(activity: ActivityLog): Promise<User> {
+  const result = await fetch(URL + 'acceptBookRequest', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(activity)
+  })
+    .then(response => response.json())
+  return result;
+}
+
+async function rejectBookRequest(obj: BookRequest): Promise<User> {
+  const result = await fetch(URL + 'rejectBookRequest', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(obj)
+  })
+    .then(response => response.json())
+  return result;
+}
+
+async function deleteMessage(activity: ActivityLog): Promise<User> {
+  const result = await fetch(URL + 'removeActivityLogElement', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(activity)
+  })
+    .then(response => response.json())
+  return result;
+}
+
+async function updateTarget(id: string, newTarget: number) {
+  return await fetch(URL + 'updateTarget', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ _id: id, target: newTarget })
+  })
+    .then(response => response.json())
+}
+
+export {
+  createUser,
+  getUser,
+  getFriendName,
+  acceptFriend,
+  rejectFriend,
+  requestBook,
+  addBook,
+  acceptBookRequest,
+  rejectBookRequest,
+  deleteMessage,
+  searchFriend,
+  addFriend,
+  getAvailableBooks,
+  updateTarget,
+  deleteBook,
+  updateBook
+}
---
+++ client/src/App.tsx
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from 'react';
+import UnauthenticatedApp from './UnauthenticatedApp'
+import AuthenticatedApp from './AuthenticatedApp'
+import {
+  getUser,
+  acceptFriend,
+  rejectFriend,
+  acceptBookRequest,
+  rejectBookRequest,
+  deleteMessage,
+  addFriend,
+  updateTarget,
+  addBook,
+  deleteBook,
+  updateBook
+} from './ApiService/serverApiService'
+import { ActivityLog, User, AddFriend, BookRequest, NewBook, Book } from './Interfaces'
+import './App.scss';
+
+function App() {
+  const [userLoggedIn, setUserLoggedIn] = useState<any>()
+
+  async function getUserData(name: string) {
+    const user: User = await getUser(name);
+    setUserLoggedIn(user);
+  }
+
+  // TODO: function that saves all friends books available to borrow to state
+
+  useEffect(() => {
+
+    //NEED TO HARD CODE USER AT THE MOMENT
+    getUserData('Adam')
+    //NEED TO HARD CODE USER AT THE MOMENT
+  }, [])
+
+  // function to add friend
+  const handleAddFriend = async (obj: AddFriend) => {
+    const result: User = await addFriend(obj);
+    setUserLoggedIn(result)
+  }
+
+  // function to confirm friend
+  const confirmFriend = async (activity: ActivityLog) => {
+    const result: User = await acceptFriend(activity);
+    setUserLoggedIn(result)
+  }
+  // function to reject friend request
+  const rejectFriendRequest = async (activity: ActivityLog) => {
+    const result: User = await rejectFriend(activity);
+    setUserLoggedIn(result)
+  }
+  // function to confirm book request
+  const confirmBookReq = async (activity: ActivityLog) => {
+    const result: User = await acceptBookRequest(activity);
+    setUserLoggedIn(result)
+  }
+  // function to reject book request
+  const rejectBookReq = async (obj: BookRequest) => {
+    const result: User = await rejectBookRequest(obj);
+    setUserLoggedIn(result)
+  }
+
+  //function to remove messages with no actions from dashboard
+  const removeMessage = async (activity: ActivityLog) => {
+    const result: User = await deleteMessage(activity);
+    setUserLoggedIn(result)
+  }
+
+  // update target
+  const updateYearlyTarget = async (id: string, newTarget: number) => {
+    const result: User = await updateTarget(id, newTarget);
+    setUserLoggedIn(result)
+  }
+
+  // add book
+  const addBookToBookCase = async (newbook: NewBook) => {
+    const result: Array<Book> = await addBook(newbook);
+    setUserLoggedIn({ ...userLoggedIn, books: result })
+  }
+
+  const removeBookFromBookCase = async (userId: string, bookId: string) => {
+    const result: User = await deleteBook(userId, bookId);
+    setUserLoggedIn(result)
+  }
+
+  const editBook = async (userId: string, bookId: string, newBook: Book) => {
+    const result: User = await updateBook(userId, bookId, newBook)
+    setUserLoggedIn(result)
+  }
+
+  return (
+    <div>
+      { userLoggedIn &&
+        <AuthenticatedApp
+          user={userLoggedIn}
+          confirmFriend={confirmFriend}
+          rejectFriendRequest={rejectFriendRequest}
+          confirmBookReq={confirmBookReq}
+          rejectBookReq={rejectBookReq}
+          removeMessage={removeMessage}
+          handleAddFriend={handleAddFriend}
+          updateYearlyTarget={updateYearlyTarget}
+          addBookToBookCase={addBookToBookCase}
+          removeBookFromBookCase={removeBookFromBookCase}
+          editBook={editBook}
+        />}
+      { !userLoggedIn && <UnauthenticatedApp />}
+    </div>
+  );
+}
+
+export default App;
